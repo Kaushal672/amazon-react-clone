8861beef7a5b0b6dfbe179143449c495ac3873f0
Kaushal <kbhandary672@gmail.com>
2023-08-03 21:22:31 +0530

    add button mode

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,36 @@
+import { Link } from 'react-router-dom';
+
 import classes from './Button.module.css';
 
-const Button = function ({ type, children, onClick = () => {} }) {
+const Button = function ({
+    mode = 'button',
+    type,
+    children,
+    onClick = () => {},
+    disabled = false,
+    to = '',
+    value = '',
+    name = '',
+}) {
     return (
-        <button
-            type={type || 'button'}
-            className={classes.btn}
-            onClick={() => onClick()}>
-            {children}
-        </button>
+        <>
+            {mode === 'button' && (
+                <button
+                    value={value}
+                    name={name}
+                    disabled={disabled}
+                    type={type || 'button'}
+                    className={classes.btn}
+                    onClick={() => onClick()}>
+                    {children}
+                </button>
+            )}
+            {mode === 'link' && (
+                <Link to={to} className={classes.btn} onClick={() => onClick()}>
+                    {children}
+                </Link>
+            )}
+        </>
     );
 };
 
