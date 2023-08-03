95f683c77b6339c1d181ff44ddd5a55b9eeb4dba
Kaushal <kbhandary672@gmail.com>
2023-08-03 22:01:09 +0530

    use use react router action to fetch address

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { Link, useFetcher } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { useSelector } from 'react-redux';
+import { addressActions } from '../../store';
+
 import Backdrop from '../Backdrop/Backdrop';
 import Button from '../Button/Button';
 import ModalCard from '../ModalCard/ModalCard';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import classes from './Modal.module.css';
 
-const Modal = function (props) {
-    const [pinCode, setPinCode] = useState('');
-    const [showError, setShowError] = useState(false);
+const Modal = ({ onConfirm, title, address }) => {
+    const fetcher = useFetcher();
+    const { data, state, Form } = fetcher;
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
@@ -31,31 +36,33 @@ const Modal = function (props) {
             });
     };
 
-    const ModalOverlay = (props) => {
+    const ModalOverlay = ({ title, onConfirm }) => {
         return (
-            <ModalCard title={props.title} onConfirm={props.onConfirm}>
+            <ModalCard title={title} onConfirm={onConfirm}>
                 <p className={classes['desc']}>
                     Select a delivery location to see product availability and
                     delivery options
                 </p>
-                <Button>Sign in to see your address</Button>
                 <div className={classes.container}>
                     <span className={classes.line}></span>
                     <span className={classes.text}>or enter your pincode</span>
                     <span className={classes.line}></span>
                 </div>
-                <form
-                    className={classes['form-control']}
-                    onSubmit={formSubmitHandler}>
-                    <input
-                        type='text'
-                        name='pincode'
-                        value={pinCode}
-                        onChange={(e) => setPinCode(e.target.value)}
-                    />
-                    <Button type='submit'>Apply</Button>
-                    {showError && <p>Please enter valid pincode</p>}
-                </form>
+                {state !== 'idle' && <LoadingSpinner />}
+                {state === 'idle' && (
+                    <>
+                        <Form
+                            className={classes['form']}
+                            method='post'
+                            action='/address'>
+                            <input type='text' name='pincode' />
+                            <Button type='submit'>Apply</Button>
+                        </Form>
+                        {data && data[0].Status !== 'Success' && (
+                            <p className={classes.error}>{data[0].Message}</p>
+                        )}
+                    </>
+                )}
             </ModalCard>
         );
     };
@@ -63,14 +70,11 @@ const Modal = function (props) {
     return (
         <>
             {ReactDOM.createPortal(
-                <Backdrop onConfirm={props.onConfirm} />,
+                <Backdrop onConfirm={onConfirm} />,
                 document.getElementById('backdrop-root')
             )}
             {ReactDOM.createPortal(
-                <ModalOverlay
-                    onConfirm={props.onConfirm}
-                    title={props.title}
-                />,
+                <ModalOverlay onConfirm={onConfirm} title={title} />,
                 document.getElementById('overlay-root')
             )}
         </>
@@ -78,3 +82,15 @@ const Modal = function (props) {
 };
 
 export default Modal;
+
+export async function action({ request }) {
+    const data = await request.formData();
+    const pinCode = data.get('pincode');
+
+    const res = await fetch('https://api.postalpincode.in/pincode/' + pinCode);
+    if (!res.ok) {
+        return res;
+    }
+
+    return res;
+}
