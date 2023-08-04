10b507e74bc38d4c0f225919c05534ec70995450
Kaushal <kbhandary672@gmail.com>
2023-08-04 10:49:01 +0530

    add Error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,72 @@
+import { useEffect } from 'react';
+import { useRouteError, useLocation, useNavigate } from 'react-router-dom';
+
+import MainNavBar from '../Layout/MainNavBar/MainNavBar';
+import Footer from '../Layout/Footer/Footer';
+import BGWhite from '../Helper/BGWhite';
+import classes from './Error.module.css';
+
+const Error = () => {
+    const error = useRouteError();
+    let message = 'Something went wrong!!';
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (error.status === 403 && pathname === '/orders') {
+            navigate('/address', {
+                state: { flash: 'No address to deliver your order!!' },
+            });
+        }
+
+        if (error.status === 401) {
+            navigate(`/auth?mode=login&returnTo=${pathname}`, {
+                state: { flash: 'You are not signed in!!' },
+            });
+        }
+    }, [error.status, pathname, navigate]);
+
+    let jsx = (
+        <img
+            src={require('../../images/Error.gif')}
+            alt='error'
+            className={classes['error-404-gif']}
+        />
+    );
+
+    if (error.status === 500) {
+        message = error.data?.message;
+        jsx = (
+            <img
+                src={require('../../images/500.webp')}
+                alt='500 error'
+                className={classes['error-404-gif']}
+            />
+        );
+    }
+
+    if (error.status === 404) {
+        message = 'Page your looking for is Not Found!!';
+        jsx = (
+            <img
+                src={require('../../images/404.gif')}
+                alt='404 error'
+                className={classes['error-404-gif']}
+            />
+        );
+    }
+
+    return (
+        <>
+            <MainNavBar />
+            <BGWhite />
+            <div className={classes['error-wrapper']}>
+                {jsx}
+                <h1 className={classes['error-title']}>{message}</h1>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Error;
