a25c97e595522667d494d742ded2d8edc454b3e8
Kaushal <kbhandary672@gmail.com>
2023-08-04 10:06:29 +0530

    modify slider card and style

--- src/components/SliderCard/SliderCard.js
+++ src/components/SliderCard/SliderCard.js
@@ -1,23 +1,26 @@
+import { Link } from 'react-router-dom';
+
 import classes from './SliderCard.module.css';
 
-const SliderCard = function ({ card, category }) {
+const SliderCard = function ({ card }) {
     return (
         <div className={classes.card}>
-            <a href='' className={classes['card-link']}></a>
             <div className={classes['product-image']}>
                 <img
-                    src={card.imageUrl}
+                    src={card.images[0].url}
                     alt={card.title}
                     className={classes['card-img']}
                 />
             </div>
             <div>
                 <span className={classes['discount-badge']}>
-                    Up to {card.discount} off
+                    Up to {card.discount}% off
                 </span>
-                <span className={classes['offer-deal']}>{category}</span>
+                <span className={classes['offer-deal']}>{card.offer}</span>
             </div>
-            <h5 className={classes['product-description']}>{card.title}</h5>
+            <Link to={`products/${card._id}`} className={classes['card-link']}>
+                <h1 className={classes['product-description']}>{card.title}</h1>
+            </Link>
         </div>
     );
 };
