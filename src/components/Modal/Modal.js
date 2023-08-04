9a3fd7d9c63293f059addab7c59555d568e1b3b0
Kaushal <kbhandary672@gmail.com>
2023-08-04 11:26:21 +0530

    add react redux state and display default address and update styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Link, useFetcher } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { useSelector } from 'react-redux';
 import { addressActions } from '../../store';
@@ -12,29 +13,21 @@ import classes from './Modal.module.css';
 
 const Modal = ({ onConfirm, title, address }) => {
     const fetcher = useFetcher();
+    const dispatch = useDispatch();
     const { data, state, Form } = fetcher;
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
         return () => (document.body.style.overflow = 'unset');
     }, []);
 
-    const formSubmitHandler = (e) => {
-        e.preventDefault();
-        setShowError(false);
-        fetch('https://api.postalpincode.in/pincode/' + pinCode)
-            .then((res) => {
-                if (!res.ok) throw new Error('Something went wrong');
-                return res.json();
-            })
-            .then((data) => {
-                if (data[0].Status !== 'Success') return setShowError(true);
-                console.log(data[0].PostOffice[0].Name);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+    useEffect(() => {
+        if (state === 'idle' && data && data[0].Status === 'Success') {
+            dispatch(addressActions.setQuickAddress(data[0].PostOffice[0]));
+            onConfirm();
+        }
+    }, [data, state, dispatch, onConfirm]);
 
     const ModalOverlay = ({ title, onConfirm }) => {
         return (
@@ -43,6 +36,36 @@ const Modal = ({ onConfirm, title, address }) => {
                     Select a delivery location to see product availability and
                     delivery options
                 </p>
+                {address && (
+                    <div className={classes['default-address']}>
+                        <span>Your default address</span>
+                        <ul>
+                            <li>{address.fullName}</li>
+                            <li>{address.addressline}</li>
+                            <li>{address.city}</li>
+                            <li>
+                                {address.state}, {address.country},
+                                {address.pincode}
+                            </li>
+                        </ul>
+                    </div>
+                )}
+                {isAuthenticated && (
+                    <Link
+                        onClick={onConfirm}
+                        to='/address'
+                        className={classes['address-link']}>
+                        Add new address
+                    </Link>
+                )}
+                {!isAuthenticated && (
+                    <Button
+                        mode={'link'}
+                        to='/auth?mode=login'
+                        onClick={onConfirm}>
+                        Sign in to see your address
+                    </Button>
+                )}
                 <div className={classes.container}>
                     <span className={classes.line}></span>
                     <span className={classes.text}>or enter your pincode</span>
