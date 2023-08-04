45636fa97f07aca8a4be70ca9926a539be796c72
Kaushal <kbhandary672@gmail.com>
2023-08-04 11:21:38 +0530

    modify to use router link

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import classes from './Card.module.css';
 
-const Card = (props) => {
+const Card = ({ title, img }) => {
     return (
         <div className={classes.card}>
-            <h2 className={classes.title}>{props.title}</h2>
-            <img
-                src={props.img}
-                alt={props.title}
-                className={classes['card-image']}
-            />
-            <a href='#' className={classes.btn}>
+            <h2 className={classes.title}>{title}</h2>
+            <img src={img} alt={title} className={classes['card-image']} />
+            <Link to={`/`} className={classes.btn}>
                 See more
-            </a>
+            </Link>
         </div>
     );
 };
