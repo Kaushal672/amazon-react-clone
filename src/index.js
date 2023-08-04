1d1b141f0aed1680f82668d6233821ac2b2fac65
Kaushal <kbhandary672@gmail.com>
2023-08-04 10:23:51 +0530

    add redux provider and helmet provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HelmetProvider } from 'react-helmet-async';
+import store, { persistor } from './store';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <HelmetProvider>
+                <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </HelmetProvider>
+        </Provider>
     </React.StrictMode>
 );
